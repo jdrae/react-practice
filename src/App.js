5836abc3082a75650f130dd7afe807f444e4d30a
jdrae <dr.kiwi.j@gmail.com>
2020-08-06 22:53:53 +0900

    Fix: TOC content list error

--- src/App.js
+++ src/App.js
@@ -1,65 +1,47 @@
 import React, {Component} from 'react';
-import './App.css';
 import TOC from './components/TOC'
 import Content from './components/Content'
 import Subject from  './components/Subject'
 
-class App extends Component{
-  constructor(props){
-    super(props); //초기화
-    this.state = {
-      selected_content_id:2,
-      mode: 'read',
-      subject: {title:'Recipe', sub: 'World Wide Recipe'},
-      welcome: {title:"Welcome", desc: "Hello"},
-      contents: [
-        {id:1, title: 'Hwachae', desc:'Hwachae is summer desert'},
-        {id:2, title: 'Bibimbap', desc:'Bibimbap is widely beloved main dish'},
-        {id:3, title: 'Bulgogi', desc: 'Bulgogi is fried meat with sweet sauce'},
-      ]
-    }
-  }
-  // props 와 state 값이 바뀌면 render() 가 호출
-  render(){ //클래스 안에 있는 함수는 function 붙지 않음
-    var _title, _desc = null;
-    if(this.state.mode === 'welcome'){
-      _title= this.state.welcome.title;
-      _desc= this.state.welcome.desc;
-    }else if(this.state.mode === 'read'){
-      var i = 1;
-      while(i<= this.state.contents.length){
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
-          _title= data.title;
-          _desc= data.desc;
+export default class App extends Component{
+    constructor(props){
+        super(props); //초기화
+        this.state = {
+          selected_content_id:2,
+          mode: 'read',
+          subject: {title:'Recipe', sub: 'World Wide Recipe'},
+          welcome: {title:"Welcome", desc: "Hello"},
+          contents: [
+            {id:1, title: 'Hwachae', desc:'Hwachae is summer desert'},
+            {id:2, title: 'Bibimbap', desc:'Bibimbap is widely beloved main dish'},
+            {id:3, title: 'Bulgogi', desc: 'Bulgogi is fried meat with sweet sauce'},
+          ]
         }
-        i= i+1;
       }
+
+    render(){
+       return(
+        <div>
+            <Subject 
+            title= {this.state.subject.title}
+            sub = {this.state.subject.sub}
+            onChangePage = {function(){
+            this.setState({mode:'welcome'})
+            }.bind(this)}
+            ></Subject>
+            
+            <TOC 
+            data={this.state.contents}
+            onChangePage = {function(id){
+            this.setState({
+                mode:'read',
+                selected_content_id: Number(id) // string 으로 들어옴
+            });
+            }.bind(this)}
+            ></TOC>
+
+        </div>
+        ); 
     }
-    return (
-      // JSX 코드
-      <div className = "App">
-        <Subject 
-        title= {this.state.subject.title}
-        sub = {this.state.subject.sub}
-        onChangePage = {function(){
-          this.setState({mode:'welcome'})
-        }.bind(this)}
-        ></Subject>
-        <TOC 
-        data={this.state.contents}
-        onChangePage = {function(id){
-          this.setState({
-            mode:'read',
-            selected_content_id: Number(id) // string 으로 들어옴
-          });
-        }.bind(this)}
-        ></TOC>
-        <Content title={_title} desc={_desc}></Content>
-      </div>
-    );
-  }
+    
 }
-
-export default App;
-
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,39 +1,33 @@
 import React, {Component} from 'react';
 
 class TOC extends Component{
-    render(){
-      var lists = [];
-      var data = this.props.data;
-      var i = 0;
-      while(i < data.length){
-        lists.push(
-        <li key={data[i].id}>
-          <a 
-            href={"/content/"+data[i].id}
-            data-id = {data[i].id} // e.target.dataset 의 변수 지정. -id 또는 -(arbitrary)
-            onClick={function(e){
-              e.preventDefault();
-              this.props.onChangePage(e.target.dataset.id); //id를 가져오기 위해
-            }.bind(this)}
-            /*
-            또는 다음과 같이 bind() 에 인자 설정.
-            onClick={function(id, e){
+    contentList(){
+      const data = this.props.data;
+      const lists = data.map((d)=>
+        <li key={d.id}>
+          <a
+            href={"/content/"+d.id}
+            data-id = {d.id}
+            onClick={function(id,e){
               e.preventDefault();
               this.props.onChangePage(id);
-            }.bind(this, data[i].id)}
-            */
+            }.bind(this,d.id)}
           >
-            {data[i].title}
+            {d.title}
           </a>
-        </li>)
-      }
-      
-
+        </li>
+      )
       return (
         <nav>
           {lists}
         </nav>
       );
+    }
+    render(){
+      return(
+        this.contentList()
+      );
+
     }
   }
 
