00d55f028cca60aebd8796dc6c6705130d5770d0
jdrae <dr.kiwi.j@gmail.com>
2020-09-01 22:37:21 +0900

    Update: react router

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,147 +1,26 @@
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom'
 import React, {Component} from 'react';
-import './App.css';
-import axios from 'axios';
-import {BrowserRouter, Route} from 'react-router-dom'
+
+import {Home, Test} from './inc'
 
 class App extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      host: '',
-      name: '',
-      list: [],
-      update: false
+    render(){
+        return(
+            <div className = 'App'>
+                <Route path = "/" component = {Home} exact/>
+
+                <Switch>
+                    <Route path = "/test/:data" component ={Test}/>
+                    <Route path = "/test" component ={Test}/>
+                </Switch>
+                {/* Link 는 BrowserRouter 안에서만. 같은 path 의 Route 태그 있어야 */}
+                <ul>
+                    <li><Link to ='/'>Home</Link></li> 
+                    <li><Link to ='/test'>Test</Link></li>
+                </ul>
+            </div>
+        );
     }
-  }
-
-  componentDidMount(){
-    this._getHost();
-    this._getData();
-  }
-
-  _dbTest = async() =>{
-    const res = await axios.get('/api/test');
-    console.log(res.data)
-  }
-
-  _getHost = async() => {
-    const res = await axios.get('/api/host');
-    this.setState({host: res.data.host})
-  }
-
-  _addData = async(e) => {
-    const {name} = this.state;
-    e.preventDefault();
-
-    const res = await axios('/add/data',{
-      method: 'POST',
-      data: {'data':name}, //서버로 data 를 전송 -> req.body.data 로 받음
-      headers: new Headers()
-    })
-
-    if(res.data!=null) { //not working -> how to prevent null value?
-      return window.location.reload();
-    }
-  }
-
-  _nameUpdate(e){
-    this.setState({name:e.target.value})
-  }
-
-  _getData = async () =>{
-    const res = await axios.get('/get/data');
-    if(res.data[0] === undefined){
-      let cover = [];
-      cover.push(res.data);
-      return this.setState({list :cover})
-    }
-    this.setState({list:res.data});
-  }
-
-  _modify = async(el) => {
-    const modify = prompt(el.name + "을 어떤 이름으로 변경할까요?")
-    if(modify !== null){
-      const body = {
-        name: modify,
-        id: el.id
-      }
-
-      const res = await axios('/modify/data',{
-        method: 'POST',
-        data: {'modify': body},
-        headers: new Headers()
-      })
-
-      if(res.data){
-        return window.location.reload();
-      }
-    }
-  }
-
-  _delete = async(el) => {
-    const remove = window.confirm(el.name+'을 삭제하겠습니까?');
-
-    if(remove){
-      const body = {id: el.id}
-      const res = await axios('/delete/data',{
-        method:'POST',
-        data:{'delete':body},
-        headers:new Headers()
-      })
-
-      if(res.data){
-        return window.location.reload();
-      }
-    }
-  }
-
-  render(){
-    const { list } = this.state;
-
-    return(
-      <div className ='App'>
-        {/* 클라이언트-서버 연동이 안될 경우 Blog 만 표시됨. */}
-        <h3>{this.state.host} Blog</h3>
-        <br />
-        <form method='POST' onSubmit={this._addData}>
-          <input type='text' maxLength='10' onChange={(e) => this._nameUpdate(e)}/>
-          <input type='submit' value='Add' />
-        </form>
-
-        <div style={{ height : '250px', overflow : 'auto' }}>
-            <h4 style={{ color : '#ababab'}}> Teachers List </h4>
-
-              <div style={{ border : 'solid 1px black', width : '50%', marginLeft : '25%', textAlign : 'left' }}>
-                <div style={{ display : 'grid', gridTemplateColumns : '32% 35% 30%', textAlign : 'center' }}>
-                  <div> Number </div>
-                  <div> Name </div>
-                  <div> Other </div>
-                </div>
-              </div>
-
-              {list.length !== 0
-              ? list.map( (el, key) => {
-                return(
-                  <div key={key} style={{ display : 'grid', lineHeight : '40px', gridTemplateColumns : '32% 35% 20% 0%', width : '50%', marginLeft : '25%'}}>
-                    <div> {el.id} </div>
-                    <div> {el.name} </div>
-                    <div
-                      style={{ color : '#ababab' }} 
-                      onClick={() => this._modify(el)}> Modify </div>
-                    <div
-                      style={{ color : '#ababab' }} 
-                      onClick={() => this._delete(el)}> Delete </div>
-                  </div>
-                )
-              })
-            
-              : null}
-          </div>
-
-      </div>
-    )
-  }
-
 }
 
 export default App;
