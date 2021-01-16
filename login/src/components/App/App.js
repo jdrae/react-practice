3052fb8905c78a44247d1a166e543e0c6326e2b5
jdrae <dr.kiwi.j@gmail.com>
2021-01-17 02:59:07 +0900

    sessionstorage

--- login/src/components/App/App.js
+++ login/src/components/App/App.js
@@ -1,17 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from '../Login/Login';
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
 
+function setToken(userToken){
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken(){
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token // chaining operateor ?. because first access the token will be undefined
+}
+
 function App() {
-  const [token, setToken] = useState();
+  const token = getToken();
 
   if(!token){
     return <Login setToken={setToken}/>
   }
-
   return (
     <div className="wrapper">
       <h1>Application</h1>
