9b682a0c695ba4e468d8597d63e09474ed0d24cf
jdrae <dr.kiwi.j@gmail.com>
2021-01-17 03:06:29 +0900

    custom token hook

--- login/src/components/App/App.js
+++ login/src/components/App/App.js
@@ -4,19 +4,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from '../Login/Login';
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
-
-function setToken(userToken){
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
-
-function getToken(){
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token // chaining operateor ?. because first access the token will be undefined
-}
+import useToken from './useToken';
 
 function App() {
-  const token = getToken();
+  const {token, setToken} = useToken();
 
   if(!token){
     return <Login setToken={setToken}/>
